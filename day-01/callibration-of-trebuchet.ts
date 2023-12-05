e88b68c73599d624a5809c830ee0b04a2f9b0595
Łukasz Pietrek <lpietrek@gmail.com>
2023-12-05 20:39:33 +0100

    Day 01

---
+++ day-01/callibration-of-trebuchet.ts
@@ -0,0 +1,32 @@
+const input = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`;
+
+const min = "0".charCodeAt(0);
+const max = "9".charCodeAt(0);
+console.log('codes of 0 and 9', {min, max})
+
+const sum =  input.split('\n').map(line => {
+    console.assert(line.length > 0, "lines should have at least 2 signs");
+    let left: string;
+    let right: string;
+
+    for (let i =0; i < line.length; i++) {
+        const code = line.charCodeAt(i);
+       if (min <= code && code <= max) {
+           left = line[i];
+           break;
+       }
+    }
+    for (let i =line.length - 1; i >= 0; i--) {
+        const code = line.charCodeAt(i);
+        if (min <= code && code <= max) {
+            right = line[i];
+            break;
+        }
+    }
+    return Number(left + right);
+}).reduce((s, c) => s + c, 0);
+
+console.log({sum})
